f54d6e25e581963663818c17a64f8e446b0f6d6f
shadow45921 <zealdem@gmail.com>
2025-07-10 12:30:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,1 @@
-function handleLogin(event) {
-    event.preventDefault(); // Prevent the default form submission
 
-    // Get the username and password from the form
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // You can add additional validation here if needed
-
-    // Redirect to the actual Roblox login page
-    window.location.href = 'https://www.roblox.com/login';
-}
